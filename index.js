24f439ee499349e890eac6f108856aee965876fe
Vanessa Bell <vanessa.bell14@gmail.com>
2017-02-17 14:11:38 -0800

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
-const port = 2587
+const port = process.env.PORT || 2587
 
 app.engine('.hbs', exphbs({
 	defaultLayout: 'main',
