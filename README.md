f276a5f0854453917fac5498736084beae9d43b1
Vanessa Bell <vanessa.bell14@gmail.com>
2017-02-17 13:19:24 -0800

    initial specs met: list of selectable cats and img,name,and # of clicks displayed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanessa's Cat Clicker
+
+This project is inspired by the assignment in Udacity's [Javascript Design Patterns course](https://www.udacity.com/course/javascript-design-patterns--ud989).
---
+++ js/app.js
@@ -0,0 +1,99 @@
+$(function() {
+	var model = {
+		init: function() {
+			this.cats = [
+				{name: 'Beebo', clicks: 0, img:'http://static.boredpanda.com/blog/wp-content/uploads/2015/06/pallas-cat-manul-7__880.jpg'},
+				{name: 'Jonny', clicks: 0, img:'https://newbloggycat.files.wordpress.com/2015/07/funny-karate-cat.jpg'},
+				{name: 'Susie', clicks: 0, img: "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg"},
+				{name: 'Ron', clicks: 0, img: "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg"},
+				{name: 'Boothang', clicks: 0, img: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"}
+			];
+		},
+		incrementClicks: function(name) {
+			for(var i = 0; i < this.cats.length; i++) {
+				if(this.cats[i].name === name) {
+					this.cats[i].clicks++;
+				}
+			}
+		},
+		getCats: function() {
+			return this.cats;
+		},
+		getOneCat: function(cat) {
+			for(var i = 0; i < this.cats.length; i++) {
+				if(this.cats[i].name === cat) {
+					return this.cats[i];
+				}
+			}
+		}
+	};
+
+	var controller = {
+		init: function() {
+			model.init();
+			catView.init();
+			catList.init();
+		},
+		getCats: function() {
+			return model.getCats();
+		},
+		getOneCat: function(cat){
+			return model.getOneCat(cat);
+		},
+		incrementClicks: function(name) {
+			model.incrementClicks(name);
+		}
+	};
+
+	var catView = {
+		init: function() {
+			this.catImage = $('#cat-image');
+			this.catName = $('#cat-name');
+			this.catClicks = $('#cat-clicks');
+			var cats = controller.getCats();
+			catView.render(cats[0].name);
+			this.catImage.on('click', function() {
+					var catImage = this;
+					var name = catImage.name;
+					controller.incrementClicks(name);
+					catView.render(name);
+			});
+		},
+		render: function(cat) {
+				var cat = controller.getOneCat(cat);
+				this.catImage.attr("src",cat.img);
+				this.catImage.attr("name",cat.name);
+				this.catName.html(cat.name);
+				if(cat.clicks === 1) {
+					this.catClicks.html(cat.name + " has been clicked " + cat.clicks + " time");
+				} else {
+					this.catClicks.html(cat.name + " has been clicked " + cat.clicks + " times");
+				}
+			}
+	};
+
+	var catList = {
+		init: function() {
+			this.catList = $('#cat-list');
+			catList.render();
+			$('.collection-item').first().addClass("active");
+
+			this.catList.on('click', '.collection-item', function(){
+				$('.collection-item').removeClass("active");
+				$(this).toggleClass("active");
+				var cat = $(this).data().id;
+				catView.render(cat);
+			});
+		},
+		render: function() {
+			var htmlStr= '';
+			
+			controller.getCats().forEach(function(cat) {
+				htmlStr += '<a href="#!" class="collection-item" data-id="' + cat.name + '">' + cat.name + '</a>'
+			});
+			this.catList.html(htmlStr);
+		}
+
+	};
+	controller.init();
+});
