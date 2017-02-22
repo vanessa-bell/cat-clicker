c39836c54a65ad30cbdd45e3591ef461101fd3df
Vanessa Bell <vanessa.bell14@gmail.com>
2017-02-22 10:00:27 -0800

    loading img assets locally, cleaned up code, cleaned up appearance

--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,48 @@
 $(function() {
+	/* ======== Model ========= */
 	var model = {
 		currentCat: null,
 		cats: [
-				{name: 'Beebo', clicks: 0, img:'http://static.boredpanda.com/blog/wp-content/uploads/2015/06/pallas-cat-manul-7__880.jpg', imgAttribution: "http://www.boredpanda.com/" },
-				{name: 'Jonny', clicks: 0, img:'https://newbloggycat.files.wordpress.com/2015/07/funny-karate-cat.jpg', imgAttribution: 'https://newbloggycat.com/'},
-				{name: 'Susie', clicks: 0, img: "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg", imgAttribution: 'https://www.petfinder.com/cats/bringing-a-cat-home/bringing-home-new-cat/99233806-bringing-home-new-cat-632x475/'},
-				{name: 'Ron', clicks: 0, img: "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg", imgAttribution: 'https://www.pexels.com/'},
-				{name: 'Boothang', clicks: 0, img: "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg", imgAttribution: 'http://www.rd.com/advice/pets/how-to-decode-your-cats-behavior/'}
+				{name: 'Beebo', clicks: 0, img:'img/pallas-cat-manul.jpg', imgAttribution: "http://www.boredpanda.com/" },
+				{name: 'Jonny', clicks: 0, img:'img/funny-karate-cat.jpg', imgAttribution: 'https://newbloggycat.com/'},
+				{name: 'Susie', clicks: 0, img: "img/bringing-home-new-cat.jpg", imgAttribution: 'https://www.petfinder.com/cats/bringing-a-cat-home/bringing-home-new-cat/99233806-bringing-home-new-cat-632x475/'},
+				{name: 'Ron', clicks: 0, img: "img/pexels-photo-126407.jpeg", imgAttribution: 'https://www.pexels.com/'},
+				{name: 'Boothang', clicks: 0, img: "img/cat-wants-to-tell-you-laptop.jpg", imgAttribution: 'http://www.rd.com/advice/pets/how-to-decode-your-cats-behavior/'}
 			],
-		
-		incrementClicks: function(name) {
-			for(var i = 0; i < this.cats.length; i++) {
-				if(this.cats[i].name === name) {
-					this.cats[i].clicks++;
-				}
-			}
-		},
-		getCats: function() {
-			return this.cats;
-		},
-		getOneCat: function(cat) {
-			for(var i = 0; i < this.cats.length; i++) {
-				if(this.cats[i].name === cat) {
-					return this.cats[i];
-				}
-			}
-		}
 	};
+
+	/* ======== Controller ========= */
 
 	var controller = {
 		init: function() {
+			// set currentCat to first cat in the list
+			model.currentCat = model.cats[0];
+
+			// initialize views
 			catView.init();
 			catList.init();
 		},
+
+		getCurrentCat: function() {
+			return model.currentCat;
+		},
+
 		getCats: function() {
-			return model.getCats();
+			return model.cats;
 		},
-		getOneCat: function(cat){
-			return model.getOneCat(cat);
+
+		setCurrentCat: function(catName){
+			for(var i = 0; i < model.cats.length; i++){
+				if(model.cats[i].name===catName){
+					return model.currentCat = model.cats[i];
+				}
+			}
 		},
-		incrementClicks: function(name) {
-			model.incrementClicks(name);
-		}
+
+		incrementClicks: function() {
+			model.currentCat.clicks++;
+			catView.render();
+		},
 	};
 
 	var catView = {
@@ -50,25 +51,26 @@ $(function() {
 			this.catName = $('#cat-name');
 			this.catClicks = $('#cat-clicks');
 			this.figcaption = $('#figcaption'); 
-			var cats = controller.getCats();
-			catView.render(cats[0].name);
+			
 			this.catImage.on('click', function() {
 					var catImage = this;
-					var name = catImage.name;
-					controller.incrementClicks(name);
+					controller.incrementClicks();
 					catView.render(name);
 			});
+
+			this.render();
 		},
-		render: function(cat) {
-				var cat = controller.getOneCat(cat);
+
+		render: function() {
+				var cat = controller.getCurrentCat();
 				this.catImage.attr("src",cat.img);
 				this.catImage.attr("name",cat.name);
 				this.catName.html(cat.name);
-				this.figcaption.html('Image courtesy of: <a href="' + cat.imgAttribution + '">' + cat.imgAttribution + '</a>');
+				this.figcaption.html('Image courtesy of: <a href="' + cat.imgAttribution + '" target="_blank">' + cat.imgAttribution + '</a>');
 				if(cat.clicks === 1) {
-					this.catClicks.html(cat.name + " has been clicked " + cat.clicks + " time");
+					this.catClicks.html("You have clicked " + cat.name + " " + cat.clicks + " time");
 				} else {
-					this.catClicks.html(cat.name + " has been clicked " + cat.clicks + " times");
+					this.catClicks.html("You have clicked " + cat.name + " " + cat.clicks + " times");
 				}
 			}
 	};
@@ -76,16 +78,19 @@ $(function() {
 	var catList = {
 		init: function() {
 			this.catList = $('#cat-list');
-			catList.render();
+			this.render();
+
 			$('.collection-item').first().addClass("active");
 
 			this.catList.on('click', '.collection-item', function(){
 				$('.collection-item').removeClass("active");
 				$(this).toggleClass("active");
 				var cat = $(this).data().id;
-				catView.render(cat);
+				controller.setCurrentCat(cat);
+				catView.render();
 			});
 		},
+
 		render: function() {
 			var htmlStr= '';
 			
